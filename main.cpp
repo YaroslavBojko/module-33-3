240b9ac4d1c52c6897cf00ef96887d758fe11fbb
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-09-23 16:45:19 +0300

    add

--- main.cpp
+++ main.cpp
@@ -1,23 +1,45 @@
 #include <iostream>
+#include <vector>
 
 template <typename T1, typename T2>
-class Pair
+class Registry
+{
+public:
+    T1 key;
+    T2 values;
+
+    Registry(T1 inKey, T2 inValues) : key(inKey), values(inValues) {};
+};
+
+template<typename T1, typename T2>
+Registry<T1, T2> add()
 {
     T1 key;
     T2 values;
 
-public:
-    Pair(T1 inKey, T2 inValues) : key(inKey), values(inValues) {};
-
-    void print ()
-    {
-        std::cout << key << " " << values;
-    }
-};
+    std::cout << "Enter key: ";
+    std::cin >> key;
+    std::cout << "Enter values: ";
+    std::cin >> values;
+    Registry<T1, T2> registry(key, values);
+    return registry;
+}
 
 int main() {
-    Pair<int, int> p(5, 10);
+    std::vector<Registry<int, std::string>> pairs;
 
-    p.print();
+    std::string command;
+    while (command != "exit")
+    {
+        std::cout << "Enter the command (add, remove, print, find, exit): ";
+        std::cin >> command;
 
+        if (command == "add") pairs.push_back(add<int, std::string>());
+    }
+
+
+    for (int i = 0; i < pairs.size(); ++i)
+    {
+        std::cout << pairs[i].key << " " << pairs[i].values << std::endl;
+    }
 }
