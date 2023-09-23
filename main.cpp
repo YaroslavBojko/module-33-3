0b4e05e94b74b7ad93fafb84a4e6824dc45352c1
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-09-23 08:36:49 +0300

    pair

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
-template <typename T>
+template <typename T1, typename T2>
+class Pair
+{
+    T1 key;
+    T2 values;
+
+public:
+    Pair(T1 inKey, T2 inValues) : key(inKey), values(inValues) {};
+
+    void print ()
+    {
+        std::cout << key << " " << values;
+    }
+};
 
 int main() {
+    Pair<int, int> p(5, 10);
+
+    p.print();
 
 }
